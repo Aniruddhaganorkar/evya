9dc8264bfc9b45d5eabf09c07a33a671d7d06287
Aniruddha Ganorkar <39210618+Aniruddhaganorkar@users.noreply.github.com>
2024-07-03 12:51:52 +0530

    Update index.ts cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,23 +12,15 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-    origin: ['https://evya-frontend.vercel.app'],
-    methods: ['POST', 'PUT', 'GET', 'DELETE'],
-    credential: true
-}));
-app.options('*', cors({
+const corsOptions = {
   origin: 'https://evya-frontend.vercel.app',
-  methods: ['POST', 'PUT', 'GET', 'DELETE'],
-  credentials: true
-}));
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://evya-frontend.vercel.app");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
 
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
 // app.use(cors());
 app.use(helmet()); // Add security headers
 
