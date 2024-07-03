d9eff3ebe0567e494a6c3d735ac7a2d55df3330f
Aniruddha Ganorkar <39210618+Aniruddhaganorkar@users.noreply.github.com>
2024-07-03 13:14:38 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors(corsOptions));
+app.use(cors({ origin: '*' }));
 // app.options('*', cors(corsOptions)); 
 // app.use(cors());
 app.use(helmet()); // Add security headers
