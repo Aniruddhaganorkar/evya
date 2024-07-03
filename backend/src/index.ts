685d5d39c36b17fa24d36c5167f7e1416713ce7d
Aniruddha Ganorkar <39210618+Aniruddhaganorkar@users.noreply.github.com>
2024-07-03 12:00:58 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,11 +12,12 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-    origin: ['https://evya-frontend.vercel.app'],
-    methods: ['POST', 'PUT', 'GET', 'DELETE'],
-    credential: true
-}));
+// app.use(cors({
+//     origin: ['https://evya-frontend.vercel.app'],
+//     methods: ['POST', 'PUT', 'GET', 'DELETE'],
+//     credential: true
+// }));
+app.use(cors());
 app.use(helmet()); // Add security headers
 
 // Routes
