a31ede37f89be6df63cd3e07b3ae45d355b82554
Aniruddha Ganorkar <39210618+Aniruddhaganorkar@users.noreply.github.com>
2024-07-03 13:06:37 +0530

    Update index.ts now

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,8 +19,18 @@ const corsOptions = {
   credentials: true,
 };
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://evya-frontend.vercel.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); 
+// app.options('*', cors(corsOptions)); 
 // app.use(cors());
 app.use(helmet()); // Add security headers
 
